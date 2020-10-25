15f00aa7987b20e9579e5d83e3d53063f28c2d25
Lukas <lukas.tamulaitis@mif.stud.vu.lt>
2020-10-25 16:18:24 +0200

    Added data collection for most 'frequently visited shops' pie-chart.

---
+++ Forms/UserForm.cs
@@ -0,0 +1,38 @@
+﻿using PSIShoppingEngine.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSIShoppingEngine.Forms
+{
+    public partial class UserForm : Form
+    {
+        public struct Shop
+        {
+            public string ShopName { get; set; }
+            public int shopID { get; set; }
+            public int ReceiptCount { get; set; }
+        }
+
+        public List<Shop> shops = new List<Shop>();
+
+        public UserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserForm_Load(object sender, EventArgs e)
+        {
+            //Retrieve most frequent shops info:
+            UserHelper.RetrieveShopsInfo(shops);
+
+
+        }
+    }
+}
