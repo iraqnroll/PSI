8aff4c992f10ec3591214206f3486604f4be7212
Kristupas Šinkūnas <kristupas.sinkunas@mif.stud.vu.lt>
2020-10-25 15:19:27 +0200

    Added login and registration features

---
+++ Forms/Register.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PSIShoppingEngine.Forms
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sign up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Sign_Up_Button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sign in";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Sign_In_Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(311, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(311, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Confirm password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.Type_Username);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(314, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.Type_Password);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(314, 217);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.Confirm_Password);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.Load += new System.EventHandler(this.Register_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+    }
+}
---
+++ Forms/Register.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PSIShoppingEngine.Classes;
+using PSIShoppingEngine.Forms;
+using System.Data.SQLite;
+
+namespace PSIShoppingEngine.Forms
+{
+    public partial class Register : Form
+    {
+        public string userName { get; set; }
+        public string userPassword { get; set; }
+        public string confirmUserPassword { get; set; }
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+            string SQLiteEx = null;
+
+            if (DbHelper.ValidateDB())
+            {
+                DbHelper.OpenConnection();
+
+                if (DbHelper.myConnection != null)
+                {
+                    statusStrip1.Text = "Connection sucessful.";
+                }
+                else
+                {
+                    statusStrip1.Text = SQLiteEx;
+                }
+            }
+
+            else
+            {
+                statusStrip1.Text = "Failed to locate the database, creating a new one.";
+
+                DbHelper.OpenConnection();
+
+                if (DbHelper.myConnection != null)
+                {
+                    statusStrip1.Text = "New database created. Connection sucessful.";
+                }
+                else
+                {
+                    statusStrip1.Text = SQLiteEx;
+                }
+            }
+        }
+
+        private void Type_Username(object sender, EventArgs e)
+        {
+            userName = textBox1.Text;
+        }
+
+        private void Type_Password(object sender, EventArgs e)
+        {
+            textBox2.PasswordChar = '\u25CF';
+            userPassword = textBox2.Text;
+        }
+
+        private void Confirm_Password(object sender, EventArgs e)
+        {
+            textBox3.PasswordChar = '\u25CF';
+            confirmUserPassword = textBox3.Text;
+        }
+
+        private void Sign_Up_Button_Click(object sender, EventArgs e)
+        {
+            if (confirmUserPassword != string.Empty || userPassword != string.Empty || userName != string.Empty)
+            {
+                if (userPassword == confirmUserPassword)
+                {
+                    string sqlQuery = "SELECT * FROM users WHERE username = @userName";
+                    SQLiteCommand command = new SQLiteCommand(sqlQuery, DbHelper.myConnection);
+                    command.Parameters.Add(new SQLiteParameter("@username", userName));
+                    SQLiteDataReader dr = command.ExecuteReader();
+
+                    if (dr.Read())
+                    {
+                        dr.Close();
+                        MessageBox.Show("Username already taken.");
+                    }
+
+                    else
+                    {
+                        dr.Close();
+                        string sqlQuery1 = "INSERT INTO users(username,password) VALUES(@username,@password)";
+                        SQLiteCommand command1 = new SQLiteCommand(sqlQuery1, DbHelper.myConnection);
+                        command1.Parameters.AddWithValue("username", userName);
+                        command1.Parameters.AddWithValue("password", userPassword);
+                        command1.ExecuteNonQuery();      
+                        MessageBox.Show("Account successfully created.");
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Passwords do not match.");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Check your entries.");
+            }
+        }
+
+        private void Sign_In_Button_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.ShowDialog();
+        }
+
+    }
+}
