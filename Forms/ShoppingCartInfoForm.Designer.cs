16d70482f3e6218209f74552ebad6675a7da9be2
Domantas Kemešis <kemesis123>
2020-10-26 22:05:21 +0200

    Added basic shopping cart functionality

---
+++ Forms/ShoppingCartInfoForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace PSIShoppingEngine.Forms
+{
+    partial class ShoppingCartInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddShoppingCart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddShoppingCart
+            // 
+            this.AddShoppingCart.FlatAppearance.BorderSize = 0;
+            this.AddShoppingCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddShoppingCart.Location = new System.Drawing.Point(47, 47);
+            this.AddShoppingCart.Name = "AddShoppingCart";
+            this.AddShoppingCart.Size = new System.Drawing.Size(149, 44);
+            this.AddShoppingCart.TabIndex = 0;
+            this.AddShoppingCart.Text = "Add a new shopping cart";
+            this.AddShoppingCart.UseVisualStyleBackColor = true;
+            this.AddShoppingCart.Click += new System.EventHandler(this.AddShoppingCart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(363, 47);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(343, 190);
+            this.dataGrid.TabIndex = 3;
+            // 
+            // ShoppingCartInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddShoppingCart);
+            this.Name = "ShoppingCartInfoForm";
+            this.Text = "ShoppingCartInfoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddShoppingCart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
---
+++ Forms/ShoppingCartInfoForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Org.BouncyCastle.Bcpg.Sig;
+using PSIShoppingEngine.Classes;
+
+namespace PSIShoppingEngine.Forms
+{
+    public partial class ShoppingCartInfoForm : Form
+    {
+        public ShoppingCartInfoForm()
+        {
+            InitializeComponent();
+        }
+        public List<Item> cart = new List<Item>();
+
+        private void AddShoppingCart_Click(object sender, EventArgs e)
+        {
+            cart.Clear();
+            ShoppingCartForm f = new ShoppingCartForm(cart);
+            f.Show();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Name");
+            table.Columns.Add("IKI");
+            table.Columns.Add("MAXIMA");
+            table.Columns.Add("LIDL");
+            table.Columns.Add("NORFA");
+
+            foreach (Item item in cart)
+            {
+                DataRow a = table.NewRow();
+                a["Name"] = item.ItemName;
+                a["IKI"] = DbHelper.SingleValueSelection("SELECT price, MAX (date) FROM iki JOIN products USING (product_id) WHERE product_name = '" + item.ItemName + "'", "price");
+                a["MAXIMA"] = DbHelper.SingleValueSelection("SELECT price, MAX (date) FROM maxima JOIN products USING (product_id) WHERE product_name = '" + item.ItemName + "'", "price");
+                a["LIDL"] = DbHelper.SingleValueSelection("SELECT price, MAX (date) FROM lidl JOIN products USING (product_id) WHERE product_name = '" + item.ItemName + "'", "price");
+                a["NORFA"] = DbHelper.SingleValueSelection("SELECT price, MAX (date) FROM norfa JOIN products USING (product_id) WHERE product_name = '" + item.ItemName + "'", "price");
+                // var calculated = CalculateShoppingCart(item);
+                // object[] info = { calculated[0], calculated[1], calculated[2] };
+                table.Rows.Add(a);
+            }
+                dataGrid.DataSource = table;
+                dataGrid.Columns[0].HeaderText = "Name";
+                dataGrid.Columns[1].HeaderText = "IKI";
+                dataGrid.Columns[2].HeaderText = "MAXIMA";
+                dataGrid.Columns[3].HeaderText = "LIDL";
+                dataGrid.Columns[4].HeaderText = "NORFA";
+            
+
+
+                dataGrid.Show();
+            
+
+
+        }
+        private List<String> CalculateShoppingCart(Item item)
+        {
+
+            List<String> i = new List<String>();
+            i.Add(item.ItemName);
+            var rating = new List<Tuple<string, string>>();
+
+            rating.Add(new Tuple<string, string>("iki", TestDB("iki", item)));
+            rating.Add(new Tuple<string, string>("maxima", TestDB("maxima", item)));
+            rating.Add(new Tuple<string, string>("norfa", TestDB("norfa", item)));
+            rating.Add(new Tuple<string, string>("lidl", TestDB("lidl", item)));
+            //rating.RemoveAll(t => t.Item2 == "0");
+           // rating = rating.OrderBy(t => Double.Parse(t.Item2)).ToList();
+            i.Add(rating[0].Item2);
+            i.Add(rating[0].Item1);
+
+            return i;
+        }
+        private String TestDB(String name, Item item)
+        {
+            String temp = DbHelper.SingleValueSelection
+                ("SELECT max date) FROM " + name + " JOIN products USING (product_id) WHERE product_name = '" + item.ItemName + "'", "price");
+            if (temp == "")
+                 return "0";
+             else {
+                 Regex pattern = new Regex(".");
+                 pattern.Replace(temp, ",");
+                 return temp;
+                 }
+    
+
+        }
+
+
+    }
+}
