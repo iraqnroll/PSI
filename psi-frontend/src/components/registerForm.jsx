a2caea49833dc44876d2fdb5e97c9f56d10080d2
Kristupas Šinkūnas <kristupas.sinkunas@mif.stud.vu.lt>
2020-12-19 17:58:30 +0200

    Edge case error is now handled.

--- psi-frontend/src/components/registerForm.jsx
+++ psi-frontend/src/components/registerForm.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
 import user from "../services/userService";
-import auth from "../services/authService";
 
 class RegisterForm extends Form {
     state = {
@@ -28,16 +27,17 @@ class RegisterForm extends Form {
         try {
             const response = await user.register(this.state.data);
             console.log(response);
-            //auth.loginWithJwt(response.headers["x-auth-token"]);
             window.location = "/";
         } catch (ex) {
-            if (ex.response && ex.response.status === 400) {
+
+            /*if (ex.response && ex.response.status === 400) {
                 const errors = { ...this.state.errors };
                 errors.username = ex.response.data;
                 this.setState({ errors });
-            }
-
+            }*/
             
+            alert("An account with such credentials has already been registered.");
+
         }
     };
 
