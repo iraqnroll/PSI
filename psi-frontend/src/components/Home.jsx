65a7c5d4cd264232214d4469d4969fe8b416db59
Dziugas Untulis <un.dziugas@gmail.com>
2021-02-20 15:18:09 +0200

    deleted jokeService

--- psi-frontend/src/components/Home.jsx
+++ psi-frontend/src/components/Home.jsx
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-import { getJoke } from "../services/jokeService";
 class Home extends Component {
-  async componentDidMount() {
-    const joke = await getJoke();
-    console.log(joke.content);
-    this.setState({ jokes: joke.content });
-  }
-  state = {jokes : ""};
+  async componentDidMount() { 
+  } 
   render() {
-    const { jokes } = this.state;
-    return (<div style={{ marginLeft: "25px", marginTop: "200px", textalign: "justify" }}>
-      <h1>Joke Of The Day: </h1>
-      
-      <h1 >{jokes}</h1>
+    return (<div>
       </div>);
   }
 }
